4b92eb277155748f064c62187a77c38986f0507f
JaiVon <frederickjaivon@yahoo.com>
2022-09-07 03:16:53 -0400

    Updates

--- back-end/app/controllers/admins_controller.rb
+++ back-end/app/controllers/admins_controller.rb
@@ -1,2 +1,17 @@
 class AdminsController < ApplicationController
+    def index
+        render json: Admin.all 
+    end
+
+    # get '/me'
+    def show 
+        current_admin = Admin.find(session[:admin_id])
+        if current_admin
+            render json: current_admin, serializer: MemberSerializer, status: :ok
+        else
+            render json: { errors: "No Active Sessions" }, status: :unauthorized #401
+        end
+
+    end
+
 end
